d2e624f3463e0235125bf3fe93941ce889117a1a
Teknologicus <jason.paul.weiss@gmail.com>
2020-05-27 17:33:14 -0700

    Improved usage example.

--- example.cpp
+++ example.cpp
@@ -2,12 +2,18 @@
 
 // declaration
 ImGui::ImStatefulCanvas *imCanvas;
+float x0, y0, x1, y1;
+int   id;
 ...
 // start up
 imCanvas = new ImGui::ImStatefulCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
+x0 = 0; y0 = 0; x1 = 20; y1 = 20; id = -1;
 ...
-// update
-imCanvas->line(ImVec2(0, 0), ImVec2(20, 20), 0xFF00FFFF);
+// infrequent update
+if (id != -1)
+  imCanvas->erase(id);
+
+id = imCanvas->line(ImVec2(x0, y0), ImVec2(x1, y1), 0xFF00FFFF);
 ...
 // redraw
 bool open = true;
