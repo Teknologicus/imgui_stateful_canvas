2a912b45b658049e1c3d5df7bdcc8b17504b002f
Teknologicus <jason.paul.weiss@gmail.com>
2020-05-28 07:34:52 -0700

    Made more succinct.

--- imgui_stateful_canvas.h
+++ imgui_stateful_canvas.h
@@ -35,8 +35,8 @@ class ImStatefulCanvas {
     ImStatefulCanvas(float width, float height);
     ImStatefulCanvas(float x, float y, float width, float height);
     ~ImStatefulCanvas() { clear(); }
-    void setSize(float width, float height);
-    void setLocation(float x, float y);
+    void setSize(float width, float height) { size_ = {width, height}; }
+    void setLocation(float x, float y) { useCursorPosition_ = false; location_ = {x, y}; }
     int line(const ImVec2 &p1, const ImVec2 &p2, ImU32 col, float thickness = 1.0f);
     int rect(const ImVec2 &p_min, const ImVec2 &p_max, ImU32 col, float rounding = 0.0f,
              ImDrawCornerFlags rounding_corners = ImDrawCornerFlags_All, float thickness = 1.0f);
@@ -56,28 +56,28 @@ class ImStatefulCanvas {
     int polyline(const ImVec2 *points, int num_points, ImU32 col, bool closed, float thickness = 1.0f);
     int convexPolyFilled(const ImVec2 *points, int num_points, ImU32 col); // Note: Anti-aliased filling requires points to be in clockwise order.
     int bezierCurve(const ImVec2 &p1, const ImVec2 &p2, const ImVec2 &p3, const ImVec2 &p4, ImU32 col, float thickness = 1.0f, int num_segments = 0);
-    int image(ImTextureID user_texture_id, const ImVec2& p_min, const ImVec2& p_max, const ImVec2& uv_min = ImVec2(0, 0),
-              const ImVec2& uv_max = ImVec2(1, 1), ImU32 col = IM_COL32_WHITE);
-    int imageQuad(ImTextureID user_texture_id, const ImVec2& p1, const ImVec2& p2, const ImVec2& p3, const ImVec2& p4, const ImVec2& uv1 = ImVec2(0, 0),
-                  const ImVec2& uv2 = ImVec2(1, 0), const ImVec2& uv3 = ImVec2(1, 1), const ImVec2& uv4 = ImVec2(0, 1), ImU32 col = IM_COL32_WHITE);
-    int imageRounded(ImTextureID user_texture_id, const ImVec2& p_min, const ImVec2& p_max, const ImVec2& uv_min, const ImVec2& uv_max, ImU32 col,
+    int image(ImTextureID user_texture_id, const ImVec2 &p_min, const ImVec2 &p_max, const ImVec2 &uv_min = ImVec2(0, 0),
+              const ImVec2 &uv_max = ImVec2(1, 1), ImU32 col = IM_COL32_WHITE);
+    int imageQuad(ImTextureID user_texture_id, const ImVec2 &p1, const ImVec2 &p2, const ImVec2 &p3, const ImVec2 &p4, const ImVec2 &uv1 = ImVec2(0, 0),
+                  const ImVec2 &uv2 = ImVec2(1, 0), const ImVec2 &uv3 = ImVec2(1, 1), const ImVec2 &uv4 = ImVec2(0, 1), ImU32 col = IM_COL32_WHITE);
+    int imageRounded(ImTextureID user_texture_id, const ImVec2 &p_min, const ImVec2 &p_max, const ImVec2 &uv_min, const ImVec2 &uv_max, ImU32 col,
                      float rounding, ImDrawCornerFlags rounding_corners = ImDrawCornerFlags_All);
-    void show(const char *label) const;
+    void draw(const char *label) const;
     void erase(int id);
     void clear();
     
   private:
-    struct Primitive { virtual void show(ImDrawList *drawList, float x, float y) = 0; virtual ~Primitive() { } };
-    struct Center { float center[2]; };
-    struct Point { Point() { } Point(float x, float y) { p[0] = x; p[1] = y; } float p[2]; };
-    struct Points2 { float p1[2], p2[2]; };
-    struct Points3 { float p1[2], p2[2], p3[2]; };
-    struct Points4 { float p1[2], p2[2], p3[2], p4[2]; };
+    struct Primitive { virtual void draw(ImDrawList *drawList, const ImVec2 &loc) = 0; virtual ~Primitive() { } };
+    struct Center { ImVec2 center; };
+    struct Point { ImVec2 p; };
+    struct Points2 { ImVec2 p1, p2; };
+    struct Points3 { ImVec2 p1, p2, p3; };
+    struct Points4 { ImVec2 p1, p2, p3, p4; };
     struct Points {
       Points(int size) { adjustedPoints = new ImVec2[size]; }
       virtual ~Points() { delete [] adjustedPoints; }
-      ImVector<Point> points;
-      ImVec2          *adjustedPoints;
+      ImVector<ImVec2> points;
+      ImVec2           *adjustedPoints;
     };
     struct Color { ImU32 color; };
     struct Color4 { ImU32 color1, color2, color3, color4; };
@@ -88,23 +88,23 @@ class ImStatefulCanvas {
     struct String { std::string string; };
     struct CornerFlags { ImDrawCornerFlags cornerFlags; };
     struct Texture { ImTextureID textureId; };
-    struct UV2 { float uv1[2], uv2[2]; };
-    struct UV4 { float uv1[2], uv2[2], uv3[2], uv4[2]; };
-    struct Line : Primitive, Points2, Color, Thickness { virtual void show(ImDrawList *drawList, float x, float y); };
-    struct Rect : Primitive, Points4, Color, Rounding, CornerFlags, Thickness { virtual void show(ImDrawList *drawList, float x, float y); };
-    struct RectFilled : Primitive, Points4, Color, Rounding, CornerFlags { virtual void show(ImDrawList *drawList, float x, float y); };
-    struct RectFilledMultiColor : Primitive, Points4, Color4 { virtual void show(ImDrawList *drawList, float x, float y); };
-    struct Quad : Primitive, Points4, Color, Thickness { virtual void show(ImDrawList *drawList, float x, float y); };
-    struct QuadFilled : Primitive, Points4, Color { virtual void show(ImDrawList *drawList, float x, float y); };
-    struct Triangle : Primitive, Points3, Color, Thickness { virtual void show(ImDrawList *drawList, float x, float y); };
-    struct TriangleFilled : Primitive, Points3, Color { virtual void show(ImDrawList *drawList, float x, float y); };
-    struct Circle : Primitive, Center, Radius, Color, Segments, Thickness { virtual void show(ImDrawList *drawList, float x, float y); };
-    struct CircleFilled : Primitive, Center, Radius, Color, Segments { virtual void show(ImDrawList *drawList, float x, float y); };
-    struct Ngon : Primitive, Center, Radius, Color, Segments, Thickness { virtual void show(ImDrawList *drawList, float x, float y); };
-    struct NgonFilled : Primitive, Center, Radius, Color, Segments { virtual void show(ImDrawList *drawList, float x, float y); };
-    struct Text : Primitive, Point, Color, String { virtual void show(ImDrawList *drawList, float x, float y); };
+    struct UV2 { ImVec2 uv1, uv2; };
+    struct UV4 { ImVec2 uv1, uv2, uv3, uv4; };
+    struct Line : Primitive, Points2, Color, Thickness { virtual void draw(ImDrawList *drawList, const ImVec2 &loc); };
+    struct Rect : Primitive, Points4, Color, Rounding, CornerFlags, Thickness { virtual void draw(ImDrawList *drawList, const ImVec2 &loc); };
+    struct RectFilled : Primitive, Points4, Color, Rounding, CornerFlags { virtual void draw(ImDrawList *drawList, const ImVec2 &loc); };
+    struct RectFilledMultiColor : Primitive, Points4, Color4 { virtual void draw(ImDrawList *drawList, const ImVec2 &loc); };
+    struct Quad : Primitive, Points4, Color, Thickness { virtual void draw(ImDrawList *drawList, const ImVec2 &loc); };
+    struct QuadFilled : Primitive, Points4, Color { virtual void draw(ImDrawList *drawList, const ImVec2 &loc); };
+    struct Triangle : Primitive, Points3, Color, Thickness { virtual void draw(ImDrawList *drawList, const ImVec2 &loc); };
+    struct TriangleFilled : Primitive, Points3, Color { virtual void draw(ImDrawList *drawList, const ImVec2 &loc); };
+    struct Circle : Primitive, Center, Radius, Color, Segments, Thickness { virtual void draw(ImDrawList *drawList, const ImVec2 &loc); };
+    struct CircleFilled : Primitive, Center, Radius, Color, Segments { virtual void draw(ImDrawList *drawList, const ImVec2 &loc); };
+    struct Ngon : Primitive, Center, Radius, Color, Segments, Thickness { virtual void draw(ImDrawList *drawList, const ImVec2 &loc); };
+    struct NgonFilled : Primitive, Center, Radius, Color, Segments { virtual void draw(ImDrawList *drawList, const ImVec2 &loc); };
+    struct Text : Primitive, Point, Color, String { virtual void draw(ImDrawList *drawList, const ImVec2 &loc); };
     struct Text2 : Primitive, Point, Color, String {
-      virtual void show(ImDrawList *drawList, float x, float y);
+      virtual void draw(ImDrawList *drawList, const ImVec2 &loc);
       const ImFont *font;
       float        fontSize;
       float        wrapWidth;
@@ -112,30 +112,26 @@ class ImStatefulCanvas {
     };
     struct Polyline : Primitive, Points, Color, Thickness {
       Polyline(int size) : Points(size) { }
-      virtual void show(ImDrawList *drawList, float x, float y);
+      virtual void draw(ImDrawList *drawList, const ImVec2 &loc);
       bool closed;
     };
     struct ConvexPolyFilled : Primitive, Points, Color {
       ConvexPolyFilled(int size) : Points(size) { }
-      virtual void show(ImDrawList *drawList, float x, float y);
+      virtual void draw(ImDrawList *drawList, const ImVec2 &loc);
     };
-    struct BezierCurve : Primitive, Points4, Color, Thickness, Segments { virtual void show(ImDrawList *drawList, float x, float y); };
-    struct Image : Primitive, Texture, Points2, UV2, Color { virtual void show(ImDrawList *drawList, float x, float y); };
-    struct ImageQuad : Primitive, Texture, Points4, UV4, Color { virtual void show(ImDrawList *drawList, float x, float y); };
-    struct ImageRounded : Primitive, Texture, Points2, UV2, Color, Rounding, CornerFlags { virtual void show(ImDrawList *drawList, float x, float y); };
+    struct BezierCurve : Primitive, Points4, Color, Thickness, Segments { virtual void draw(ImDrawList *drawList, const ImVec2 &loc); };
+    struct Image : Primitive, Texture, Points2, UV2, Color { virtual void draw(ImDrawList *drawList, const ImVec2 &loc); };
+    struct ImageQuad : Primitive, Texture, Points4, UV4, Color { virtual void draw(ImDrawList *drawList, const ImVec2 &loc); };
+    struct ImageRounded : Primitive, Texture, Points2, UV2, Color, Rounding, CornerFlags { virtual void draw(ImDrawList *drawList, const ImVec2 &loc); };
     typedef ImVector<Primitive *> DrawList;
 
   private:
     int addToDrawList(Primitive *primitive);
-    static ImVec2 mkImVec2(const float p[2]) { return ImVec2(p[0], p[1]); }
-    static ImVec2 mkAdjImVec2(const float p[2], float x, float y) { return ImVec2(p[0] + x, p[1] + y); }
 
   private:
     bool     useCursorPosition_;
-    float    x_,
-             y_,
-             width_,
-             height_;
+    ImVec2   location_,
+             size_;
     DrawList drawList_;
 };
 } // namespace ImGui
